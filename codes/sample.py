0ef591a5842e3f70af4616ef51cf209b47ec0b3f
Effie Gao <xtm613@usask.ca>
2022-09-15 17:08:03 -0600

    python code added

---
+++ codes/sample.py
@@ -0,0 +1,14 @@
+def sum(a,b):
+    print(a+b);
+
+sum(1,2)
+
+def greet(name):
+    """
+    This function greets to
+    the person passed in as
+    a parameter
+    """
+    print("Hello, " + name + ". Good morning!")
+
+greet('Paul')
